53ca9e8f789725dfe670b878742510d3f3d94190
s559152 <123050392+s559152@users.noreply.github.com>
2023-04-27 21:47:00 -0500

    first commit

--- routes/bank.js
+++ routes/bank.js
@@ -16,10 +16,10 @@ router.get('/', function(req, res, next) {
 router.get('/', Bank_controlers.Bank_view_all_Page );
 router.get('/Bank/:id', Bank_controlers.Bank_detail);
 router.get('/detail', Bank_controlers.Bank_view_one_Page);
-router.get('/create', Bank_controlers.Bank_create_Page);
+router.get('/create',secured, Bank_controlers.Bank_create_Page);
 router.get('/update', Bank_controlers.Bank_update_Page);
-router.get('/delete', Bank_controlers.Bank_delete_Page);
-router.get('/update', Bank_controlers.Bank_update_Page);
+router.get('/delete',secured, Bank_controlers.Bank_delete_Page);
+router.get('/update',secured, Bank_controlers.Bank_update_Page);
 router.get('/update', secured,Bank_controlers.Bank_update_Page);
 
 module.exports = router;
